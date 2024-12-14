a0ee3192127033b9dbf0a41830c94f1b01141571
floor-licker <kingofthecoast78@gmail.com>
2024-12-14 14:57:03 -0500

    Remove construction of empty std::string_view objects in vm.cpp
    I think this alternative is more readable. In the `set_option` function, these two lines handle potentially null C strings. `std::string_view{}` requires an explicit constructor call but is functionally identical to just using an empty string.

--- lib/evmone/vm.cpp
+++ lib/evmone/vm.cpp
@@ -29,8 +29,8 @@ constexpr evmc_capabilities_flagset get_capabilities(evmc_vm* /*vm*/) noexcept
 
 evmc_set_option_result set_option(evmc_vm* c_vm, char const* c_name, char const* c_value) noexcept
 {
-    const auto name = (c_name != nullptr) ? std::string_view{c_name} : std::string_view{};
-    const auto value = (c_value != nullptr) ? std::string_view{c_value} : std::string_view{};
+    const auto name = (c_name != nullptr) ? std::string_view{c_name} : "";
+    const auto value = (c_value != nullptr) ? std::string_view{c_value} : "";
     auto& vm = *static_cast<VM*>(c_vm);
 
     if (name == "advanced")
